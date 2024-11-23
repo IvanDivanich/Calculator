1ef8519fc7fadfd43bff65f9898f0ea90b492c9b
Katya <k.borisova.652@mail.ru>
2024-11-23 14:47:35 +0300

    merge all

--- Program.cs
+++ Program.cs
@@ -10,31 +10,42 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            SpecialFuns specialFuns = new SpecialFuns();
+            Math1 math1 = new Math1();
             int x, y, operand;
-            Console.WriteLine("Выберите действие\n1 - Сложение\n2 - Вычитание\n3 - Умножение\n4 - Деление\n5 - Степень");
 
-            operand = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите первое число:");
-            x = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите второе число:");
-            y = int.Parse(Console.ReadLine());
-
-            Console.WriteLine();
-
-            switch (operand)
+            while (true)
             {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
+                Console.WriteLine("Выберите действие\n1 - Сложение\n2 - Вычитание\n3 - Умножение\n4 - Деление\n5 - Степень");
+
+                operand = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите первое число:");
+                x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                y = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                switch (operand)
+                {
+                    case 1:
+                        Console.WriteLine(math1.Summary(x, y));
+                        break;
+                    case 2:
+                        Console.WriteLine(math1.Diference(x, y));
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Console.WriteLine(specialFuns.Pow(x, y));
+                        break;
+                }
+
+                Console.ReadKey();
             }
         }
     }
