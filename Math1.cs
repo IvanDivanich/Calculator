ec23b751fa8151616667080d20b808ecfb01830b
Katya <k.borisova.652@mail.ru>
2024-11-23 14:28:39 +0300

    add class Math1

---
+++ Math1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Math1
+    {
+        public double Summary(int a, int b)
+        { return a + b; }
+
+        public double Diference(int a, int b)
+        { return a - b; }
+    }
+}
